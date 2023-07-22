a73775959e1e94010cbe5b22a9d27a13d1af035b
Daniel <dphillip11@gmail.com>
2023-07-22 20:32:24 +0200

    slight refactoring, style update

--- script.js
+++ script.js
@@ -1,18 +1,11 @@
-
-
-
-        var actionInterval = 0.5;
-        var actionTime = 0;
+var actionInterval = 0.5;
+var actionTime = 0;
 var isDoingAction = false;
-        var isSlashing = false;
+var isSlashing = false;
     
 var score = 0;
 var health = 3;
 
-        function getRandom(min, max) {
-            return Math.random() * (max - min) + min;
-}
-        
 var scoreText = document.getElementById("score");
 var healthText = document.getElementById("health");
 
@@ -26,10 +19,29 @@ function ChangeHealth(amount) {
     healthText.innerHTML = "Health: " + health;
 }
 
+function GetRandom(min, max) {
+    return Math.random() * (max - min) + min;
+}
+        
 var man = document.getElementById("man");
 var whale = document.getElementById("whale");
 var seagull = document.getElementById("seagull");
 var bitcoin = document.getElementById("bitcoin");
+
+function ResetPosition(object)
+{
+    object.style.left = "110%";
+    if (object == seagull)
+        object.style.top = "40%";
+}
+
+function ResetGame() {
+    ChangeHealth(3 - health);
+    ChangeScore(-score);
+    ResetPosition(whale);
+    ResetPosition(seagull);
+    ResetPosition(bitcoin);
+}
 
 var manHitbox = document.getElementById("man_hitbox");
 var hurtbox = document.querySelector(".hurtbox");
@@ -38,27 +50,30 @@ var whaleHitbox = document.getElementById("whale_hitbox");
 var seagullHitbox = document.getElementById("seagull_hitbox");
 var bitcoinHitbox = document.getElementById("bitcoin_hitbox");
 
-var launchedObject = whale;
+var launchedObject;
 var movementSpeed = 50;
 
-function launchObject() {
+function SetRandomMovementSpeed() {
+    movementSpeed = GetRandom(50, 150);
+}
+
+function LaunchObject() {
     var objectIndex = (Math.floor)(Math.random() * 3);
     switch (objectIndex) {
         case 0: launchedObject = whale; break;
         case 1: launchedObject = seagull; break;
         case 2: launchedObject = bitcoin; break;
     }
-    launchedObject.style.left = "110%";
-    movementSpeed = getRandom(50, 150);
+    SetRandomMovementSpeed();
 }
 
 function moveLaunchedObject(distance) {
-    var x = parseFloat(launchedObject.style.left);
+    var x = launchedObject.style.left = parseFloat(launchedObject.style.left) || 110;
     x -= distance;
     launchedObject.style.left = x + "%";
     if (x < -10) {
-        resetLaunchedObject();
-        launchObject();
+        ResetPosition(launchedObject);
+        LaunchObject();
         if (launchedObject != bitcoin) {
             ChangeScore(1);
         }
@@ -74,17 +89,11 @@ function moveLaunchedObject(distance) {
     }
 }
 
-function resetLaunchedObject() {
-    launchedObject.style.left = "110%";
-    launchObject();
-}
-
 function CollideWithPlayer() {
-    resetLaunchedObject();
+    ResetPosition(launchedObject);
     ChangeHealth(-1);
     playHurtAnimation();
 }
-
 
 function checkCollision(a, b)
 {
@@ -105,40 +114,40 @@ function playHurtAnimation()
 }
 
 window.addEventListener('keydown', function (event) {
-            if (isDoingAction) {
-                return;
-            }
-            if (event.key == 'w') {
-                man.style.backgroundImage = 'url("Textures/man_jump.png")';
-                manHitbox.classList.add("jumping");
-            }
-            else if (event.key == 'd') {
-                man.style.backgroundImage = 'url("Textures/man_hit.png")';
-                hurtbox.style.display = "block";
-                isSlashing = true;
-            }
-            else if (event.key == 's') {
-                man.style.backgroundImage = 'url("Textures/man_duck.png")';
-                manHitbox.classList.add("ducking");
-            }
-            isDoingAction = true;
-        }, false);
+    if (isDoingAction) {
+        return;
+    }
+    if (event.key == 'w') {
+        man.style.backgroundImage = 'url("Textures/man_jump.png")';
+        manHitbox.classList.add("jumping");
+    }
+    else if (event.key == 'd') {
+        man.style.backgroundImage = 'url("Textures/man_hit.png")';
+        hurtbox.style.display = "block";
+        isSlashing = true;
+    }
+    else if (event.key == 's') {
+        man.style.backgroundImage = 'url("Textures/man_duck.png")';
+        manHitbox.classList.add("ducking");
+    }
+    isDoingAction = true;
+}, false);
 
 
-        function UpdateMan(dt) {
-            if (isDoingAction) {
-                actionTime += dt;
-                if (actionTime > actionInterval) {
-                    actionTime = 0;
-                    isDoingAction = false;
-                    isSlashing = false;
-                    man.style.backgroundImage = 'url("Textures/man_stand.png")';
-                    manHitbox.classList.remove("jumping");
-                    manHitbox.classList.remove("ducking");
-                    hurtbox.style.display = "none";
-                }
-            }
+function UpdateMan(dt) {
+    if (isDoingAction) {
+        actionTime += dt;
+        if (actionTime > actionInterval) {
+            actionTime = 0;
+            isDoingAction = false;
+            isSlashing = false;
+            man.style.backgroundImage = 'url("Textures/man_stand.png")';
+            manHitbox.classList.remove("jumping");
+            manHitbox.classList.remove("ducking");
+            hurtbox.style.display = "none";
         }
+    }
+}
         
 function checkCollisions() {
     var hit = false;
@@ -147,7 +156,7 @@ function checkCollisions() {
     }
     if (isSlashing && checkCollision(hurtbox, bitcoinHitbox)) {
         ChangeScore(5);
-        resetLaunchedObject();
+        ResetPosition(launchedObject);
     }
 }
         
@@ -156,23 +165,23 @@ function GameOver() {
 }
 
 function updateGame(dt) {
-            if (health <= 0) {
-                GameOver();
-            }
-            UpdateMan(dt);
-            checkCollisions();
-            moveLaunchedObject(movementSpeed * dt);
-        }
+    if (health <= 0) {
+        GameOver();
+    }
+    UpdateMan(dt);
+    checkCollisions();
+    moveLaunchedObject(movementSpeed * dt);
+}
 
-        var previousTime = Date.now();
+var previousTime = Date.now();
 
-        function gameLoop() {
-            var currentTime = Date.now();
-            var dt = (currentTime - previousTime) / 1000;
-            previousTime = currentTime;
-            updateGame(dt);
-            requestAnimationFrame(gameLoop);
-        }
+function gameLoop() {
+    var currentTime = Date.now();
+    var dt = (currentTime - previousTime) / 1000;
+    previousTime = currentTime;
+    updateGame(dt);
+    requestAnimationFrame(gameLoop);
+}
 
-        launchObject();
-        gameLoop();
+LaunchObject();
+gameLoop();
